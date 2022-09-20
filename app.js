400c7d00bc7ec9693a23d86908eded8e72ae6f22
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-09-20 22:34:52 +0400

    make script external

---
+++ app.js
@@ -0,0 +1,43 @@
+gsap.from("#m1", {
+      scrollTrigger: {
+        scrub: true
+      },
+      y: 200,
+    });
+    gsap.from("#m2", {
+      scrollTrigger: {
+        scrub: true
+      },
+      x: 100,
+    });
+    gsap.from("#t2", {
+      scrollTrigger: {
+        scrub: true
+      },
+      y: 100,
+    });
+    gsap.from("#t1", {
+      scrollTrigger: {
+        scrub: true
+      },
+      y: 25,
+    });
+    gsap.from("#man", {
+      scrollTrigger: {
+        scrub: true
+      },
+      x: -75,
+    });
+    gsap.from("#plants", {
+      scrollTrigger: {
+        scrub: true
+      },
+      x: -50,
+    });
+    gsap.from(".text", {
+      scrollTrigger: {
+        scrub: true
+      },
+      x: 450,
+      y: 100,
+    });
